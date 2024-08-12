64af2768376da4885fdbd1e506dfe5ae9f51131b
kevcodeus <126352240+kevcodeus@users.noreply.github.com>
2024-08-13 00:18:14 +0530

    Add files via upload

---
+++ Day12/ContainerWithMostWater.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r, area = 0, len(height) - 1, 0
+        while l < r:
+            area = max(area, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+				
+        return area
+height = [1,8,6,2,5,4,8,3,7]  # Example input
+solution = Solution()
+result = solution.maxArea(height)
+print(f"Maximum water the container can store: {result}")
